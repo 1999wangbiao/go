c8d742594e98804d59311440f7825e0c039df565
wangbiao <2734449588@qq.com>
2024-11-21 14:47:22 +0800

    图片列表排序

--- codeface/gvb_study/gvb_server/service/common/service_list.go
+++ codeface/gvb_study/gvb_server/service/common/service_list.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"gvb_server/global"
 	"gvb_server/models/system"
 )
@@ -10,19 +11,23 @@ type Option struct {
 	Debug bool
 }
 
-func ComList[T any](model T, option Option) (list T, count int64, err error) {
+func ComList[T any](model T, option Option) (list []T, count int64, err error) {
 
-	var imageList []T
 	//查询的总共条数
-	count = global.DB.Select("id").Find(&imageList).RowsAffected
+	count = global.DB.Select("id").Find(&list).RowsAffected
 	offset := (option.Page.Page - 1) * option.Limit
 	if offset < 0 {
 		offset = 0
 	}
+	fmt.Println(option.Limit)
+	fmt.Println(option.Page.Page)
+	if option.Sort == "" {
+		option.Sort = "created_at asc" //设置默认排序
+	}
 	if !option.Debug {
-		err = global.DB.Limit(option.Limit).Offset(offset).Find(&imageList).Error
+		err = global.DB.Limit(option.Limit).Offset(offset).Order(option.Sort).Find(&list).Error
 	} else {
-		err = global.DB.Debug().Limit(option.Limit).Offset(offset).Find(&imageList).Error
+		err = global.DB.Debug().Limit(option.Limit).Offset(offset).Order(option.Sort).Find(&list).Error
 	}
 	return list, count, err
 }
